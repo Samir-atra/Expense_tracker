fdea4eebf700dd1634419ac00f263ab522b2205f
Samer <samiratra95@gmail.com>
2023-06-02 23:21:23 +0300

    creating a module for gui_function

--- project/DBproject.py
+++ project/DBproject.py
@@ -23,32 +23,33 @@ import re
 import sys
 import textwrap
 import PySimpleGUI as sg
+from .
 
-def gui_function(win_title, text1, text2, submit, cancel, size):
-    # a function for generating the graphical user interface for the project
-    if size == 1:
-        layout = [
-            [sg.Text(text1)],
-            [sg.InputText()],
-            [sg.Submit(submit), sg.Cancel(cancel)],
-        ]
-    elif size == 2:
-        layout = [
-            [sg.Text(text1)],
-            [sg.InputText()],
-            [sg.Text(text2)],
-            [sg.InputText()],
-            [sg.Submit(submit), sg.Cancel(cancel)],
-        ]
+# def gui_function(win_title, text1, text2, submit, cancel, size):
+#     # a function for generating the graphical user interface for the project
+#     if size == 1:
+#         layout = [
+#             [sg.Text(text1)],
+#             [sg.InputText()],
+#             [sg.Submit(submit), sg.Cancel(cancel)],
+#         ]
+#     elif size == 2:
+#         layout = [
+#             [sg.Text(text1)],
+#             [sg.InputText()],
+#             [sg.Text(text2)],
+#             [sg.InputText()],
+#             [sg.Submit(submit), sg.Cancel(cancel)],
+#         ]
 
-    window = sg.Window(win_title, layout)
+#     window = sg.Window(win_title, layout)
 
-    event, values = window.read()
-    if event == "Cancel":
-        sys.exit()
-    window.close()
+#     event, values = window.read()
+#     if event == "Cancel":
+#         sys.exit()
+#     window.close()
 
-    return event, values
+#     return event, values
 
 ########################################################################################################
 
