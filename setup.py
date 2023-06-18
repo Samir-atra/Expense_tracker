a925f31a6c2a83d1cf910f3ea9c945935a2e3170
Samer <samiratra95@gmail.com>
2023-06-18 04:07:56 +0300

    creating setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='Expenses_tracker',
+    version='0.1.0',    
+    description='An expenses tracking tool',
+    url='https://github.com/Samir-atra/Expenses_tracker',
+    author='Samer Attrah',
+    author_email='',
+    license='MIT License',
+    packages=['Expenses_tracker'],
+    install_requires=['pandas',
+                      'reportlab',                     
+                      'PySimpleGUI',
+                      'Flask',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: all-audience',
+        'License :: MIT License',  
+        'Operating System :: Linux',        
+        'Programming Language :: Python :: 3.10',
+    ],
+)
